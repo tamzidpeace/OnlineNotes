ff01347e27fe858b5238e13d77a3a20d27317378
techdroweb <45241872+techdroweb@users.noreply.github.com>
2019-03-14 00:06:36 +0600

    added update api file

---
+++ Notes-Api/update-data.php
@@ -0,0 +1,57 @@
+<?php
+
+// $note = $_POST["note"];
+// $title = $_POST["title"];
+// $id =  $_POST["id"];
+
+// echo "$id";
+
+//$id = (int) $ids;
+
+$serverName = "localhost";
+$userName = "root";
+$password = "";
+$dbName = "user_db";
+
+// inatializing connection
+
+$con = new mysqli($serverName, $userName, $password, $dbName);
+
+// checking connection
+
+if($con -> connect_error) {
+  die("Connection error: " . $con -> connect_error);
+} 
+
+
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $id = $_REQUEST['id'];
+    $title = $_REQUEST['title'];
+    $note = $_REQUEST['note'];
+
+    $sql = "UPDATE notes SET title=? , note=? WHERE id=?";
+
+    $stmt = $con->prepare($sql);
+    
+    $stmt -> bind_param("ssi", $title, $note, $id);
+
+    if($stmt->execute()){
+		//making success response
+		$response['error'] = false;
+		$response['message'] = 'Note updated successfully';
+	}else{
+		//if not making failure response
+		$response['error'] = true;
+		$response['message'] = 'Please try later';
+	}
+} else {
+
+    $response['error'] = true;
+    $response['message'] = "invalid request";
+  }
+  
+  echo json_encode($response);
+
+?>
